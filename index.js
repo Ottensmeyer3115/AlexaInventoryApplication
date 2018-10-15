d0197343586eb48a359a6e8520f4d2c19455eaf3
Josiah Brouwer <josiah.brouwer@hope.edu>
2018-10-14 20:05:22 -0400

    Added comments to the code

--- index.js
+++ index.js
@@ -24,10 +24,21 @@ const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
 
 const handlers = {
+
+    /*
+     * LaunchRequest runs when Memory Manager is opened without invoking an intent
+     * After it opens, Memory Manager will wait for a response from the user.
+     */
     'LaunchRequest': function () {
         this.emit(':ask', "Memory manager is ready. What can I remember for you?");
     },
+
+    /*
+     * The HowMany function can be invoked by the user to determine how many
+     * items are being stored in the database for the user.
+     */
     'HowManyIntent': function () {
+      // Construct the request for the database
       const dynamoParams = {
             TableName: "MemoryManagerDB",
             AttributesToGet: "1",
@@ -36,31 +47,55 @@ const handlers = {
 
       console.log('Attempting to read data');
       console.log('\n');
+      // send the request to the database. The result will be stored in data
       docClient.scan(dynamoParams).promise().then(data => {
+        // respond to the user with the result of the query
         this.emit(':ask', "Memory manger is remembering " + data.Count + " items for you. Anything else?");
       }).catch(err => console.error(err));
 
     },
-    'StoreItemIntent': function () {
-        const item = this.event.request.intent.slots.object.value;
-        const location = this.event.request.intent.slots.preposition.value + " " +this.event.request.intent.slots.location.value;
-        const speechOutput = "You put " + item + " " + location + ".";
 
-        const dynamoParams = {
+    /*
+     * The StoreItem intent can be used to store an item in the database
+     * The user will specify the item and the location, and the function will
+     * store this information, along with the user's ID in the database.
+     */
+    'StoreItemIntent': function () {
+      // Get the item name and the location from user's intent invocation
+      const item = this.event.request.intent.slots.object.value;
+      const location = this.event.request.intent.slots.preposition.value + " " +this.event.request.intent.slots.location.value;
+
+      // Construct the verbal response that Alexa will give
+      const speechOutput = "You put " + item + " " + location + ".";
+
+      // Construct the database request
+      const dynamoParams = {
               TableName: "MemoryManagerDB",
               Item: {"userid":12,"name":item,"location":location}
-        };
+      };
 
-        docClient.put(dynamoParams).promise().then(data => {
-          this.emit(':ask', speechOutput +" Anything else?");
-          console.log(data);
-        }).catch(err => console.error(err));
+      // Send a request to insert the item to the database
+      docClient.put(dynamoParams).promise().then(data => {
+        // Respond to the user with the result
+        this.emit(':ask', speechOutput +" Anything else?");
+        console.log(data);
+      }).catch(err => console.error(err));
 
     },
-    'RecallItemIntent': function () {
-        const item = this.event.request.intent.slots.object.value;
 
-        const dynamoParams = {
+    /*
+     * The RecallItem intent can be used to retreive information from the
+     * database about an item the user previously stored. Alexa will give the
+     * location of the item. (future: The user will also be able to ask for
+     * more information about the item stored. Then Alexa will respond with
+     * additional information.)
+     */
+    'RecallItemIntent': function () {
+      // Get the item name from the request
+      const item = this.event.request.intent.slots.object.value;
+
+      // Construct the request for the database
+      const dynamoParams = {
               /*Key:{"userid":{"N":"12"},"name":{"S":"my keys"}},*/
               ConsistentRead: true,
               Select: "ALL_ATTRIBUTES",
@@ -74,30 +109,45 @@ const handlers = {
                   ":name":"plate"
               },
               TableName:"MemoryManagerDB"
-        };
-
-        docClient.query(dynamoParams).promise().then(data => {
+      };
+      // Send the request to find the item from the database
+      docClient.query(dynamoParams).promise().then(data => {
           var itemLocation = data.Items[0].location;
+          // Alexa responds with the location of the item
           this.emit(':ask', "This item is " + itemLocation +". Anything else?");
-        }).catch(err => console.error(err));
+      }).catch(err => console.error(err));
     },
+
+    /*
+     * The help intent is one the Built-in intents.
+     */
     'AMAZON.HelpIntent': function () {
         const speechOutput = HELP_MESSAGE;
         const reprompt = HELP_REPROMPT;
 
+        // Alexa responds with the help message and reprompts the user
         this.response.speak(speechOutput).listen(reprompt);
         this.emit(':responseReady');
     },
+
+    /*
+     * The cancel intent is one of the Built-in intents.
+     */
     'AMAZON.CancelIntent': function () {
         this.response.speak(STOP_MESSAGE);
         this.emit(':responseReady');
     },
+
+    /*
+     * The stop intent is one of the Built-in intents.
+     */
     'AMAZON.StopIntent': function () {
         this.response.speak(STOP_MESSAGE);
         this.emit(':responseReady');
     },
 };
 
+// Register the intents with alexa, so that they can be used by the user.
 exports.handler = function (event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
     alexa.APP_ID = APP_ID;
